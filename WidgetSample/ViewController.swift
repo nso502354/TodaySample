99d498eb6f2416305676274e27cfe3f7fd147da7
Sangwook Nam <nso502354@gmail.com>
2019-10-14 23:43:43 +0900

    Init

---
+++ WidgetSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WidgetSample
+//
+//  Created by Sangwook Nam on 14/10/2019.
+//  Copyright © 2019 nam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
